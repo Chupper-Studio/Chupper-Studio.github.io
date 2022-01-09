73cdcc6c3ae20ffffb97380d6c3f0c211d02ea14
chupper100 <phamminhdat223@gmail.com>
2022-01-09 15:41:23 +0700

    delete index.html

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
-    
-</body>
-</html>
+<!doctype html>
+<htm lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Chupper</title>
+        <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+        <link rel="stylesheet" href="./assets/css/style.css">
+        <link rel="stylesheet" href="./assets/css/mobile_style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="/assets/script/color.js" async></script>
+        <script defer>
+            $(function () {
+                $("#navigation").load("/assets/html/nav.html");
+            });
+        </script>
+    </head>
+
+    <body>
+        <nav id="navigation">
+
+        </nav>
+    </body>
+
+    </html>
