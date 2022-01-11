3f6addb5123d8f398ab9dc5bcbe7e32ffc091669
chupper100 <phamminhdat223@gmail.com>
2022-01-11 13:58:20 +0700

    update

---
+++ test-page/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="test-page/style.css">
+    <link rel="stylesheet" href="test-page/mobile_style.css">
+</head>
+<body>
+<nav>
+        <li class="home_button active"><a href="#">Home</a></li>
+            <ul class="nav_links">
+                <li><a class="menu-button" href="/contact/index.php">Contact</a></li>
+                <li><a class="menu-button" href="/blog/index.php">Blog</a></li>
+                
+                <li>
+                    <a href="#">More</a>
+                    <ul class="sub_menu">
+                    <li><a class="menu-button" href="/color/index.php">/color</a></li>
+                    <li><a class="menu-button" href="/test-page/index.php">/test-page</a></li>    
+                </ul>
+                </li>
+                
+            </ul>
+            <div class="burger">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+        </nav>
+        <div class="content">
+            <h1 class="title">About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ipsum sed quae, corrupti sint doloribus suscipit earum corporis accusamus quas magni cupiditate quia est error a odit! Repudiandae et dolores impedit magnam soluta sapiente cupiditate modi quod exercitationem atque? Voluptatum corrupti maxime ad facilis ipsam nulla dolorum vitae alias laborum, optio, consectetur ex fuga, quis magni? Quas illo repudiandae quo unde excepturi alias iure. Id nostrum, distinctio deleniti voluptatibus temporibus perferendis doloremque quos reprehenderit similique voluptatem adipisci non cumque illum nihil explicabo excepturi voluptatum est dolore quod quibusdam natus quia vitae. Dolor ad dolorem fugit repudiandae est quis officia tenetur!</p>
+        </div>
+</body>
+</html>
